4932edc8d60ea5c8302094551ed931a19e2192ee
leei8 <ikaslea@gela07_01>
2017-03-27 12:36:06 +0200

    Parametroak jarrita

--- src/modelo/Config.java
+++ src/modelo/Config.java
@@ -8,6 +8,11 @@ package modelo;
  * 25 mar. 2017
  */
 public interface Config {
+	
+	public static final String BBDD = "biltegia";
+	public static final String HOST = "localhost";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
 
 	
 }
