33e1920652188f8d77473e1e4cb30170cfb67f27
Leire <ikaslea@gela07_01>
2017-03-27 13:26:25 +0200

    Konponduta

--- src/modelo/Conector.java
+++ src/modelo/Conector.java
@@ -3,10 +3,48 @@
  */
 package modelo;
 
+import java.sql.DriverManager;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author enautirakasle
  * 25 mar. 2017
  */
 public class Conector {
+	protected Connection conexion;
 
+	Conector() {
+		
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD + "," + Config.USERNAME + "," + Config.PASSWORD);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
 }
